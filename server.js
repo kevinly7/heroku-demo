3d3205cebe3dc5bd6c1b971e55e63bcffd8dad41
Kevin Ly <kevinly7@uw.edu>
2014-12-01 10:59:13 -0800

    fixed errors

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.get('/api/send', function (req, res){
 var port = process.env.YOUR_PORT || process.env.PORT || 8080;
 var host = process.env.YOUR_HOST || '0.0.0.0';
 
-var server = app.listen('8080', function() {
+var server = app.listen(port, host, function() {
     console.log('listening for requests sent to http://localhost:%s', server.address().port);
 });
